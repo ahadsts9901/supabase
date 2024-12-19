c026aa25b2e872556fc9b7c14dcf23eb303eead9
ahadsts9901 <ahadsts990@gmail.com>
2024-12-19 13:54:31 +0500

    crud

--- src/pages/components/home/Todo.tsx
+++ src/pages/components/home/Todo.tsx
@@ -21,6 +21,18 @@ const Todo = ({ data, get }: any) => {
     const updateTodo = async () => {
         const id = data?.id
         if (!id) return
+        const updatedTodo = prompt("Update todo", data?.todo)
+        if (!updatedTodo) return
+        const { error } = await supabase
+            .from('todos')
+            .update({ todo: updatedTodo })
+            .eq('id', data?.id)
+        if (error) {
+            alert(error?.message)
+            return
+        }
+        get()
+
     }
 
     return (

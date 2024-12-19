84b05efb3c61daac7d19f7414c28af2a71ea915f
ahadsts9901 <ahadsts990@gmail.com>
2024-12-19 14:40:44 +0500

    done

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const apiKey = "https://gvnnvcctigihmvozjbtd.supabase.co"
-const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2bm52Y2N0aWdpaG12b3pqYnRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3Mjc2NzMsImV4cCI6MjA0OTMwMzY3M30.B9Mz5i7dSJEnLKS95E-lTQOY8b0efEWOqHlb08o1n6g"
+const apiKey = "your supabase api key"
+const anonKey = "your supabase anon key"
 
 export const supabase = createClient(apiKey, anonKey);

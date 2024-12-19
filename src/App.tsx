864437296d61ab3e75c1ae97c008f9f89c325678
ahadsts9901 <ahadsts990@gmail.com>
2024-12-19 11:59:43 +0500

    home

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import "./App.css"
 import { Navigate, Route, Routes } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
+import Home from "./pages/Home"
 
 const App = () => {
+
+  
 
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate to="/login" replace={true} />} />
     </Routes>
   )

243979f1cd1c763c1707fd6e7357a32e2159505a
ahadsts9901 <ahadsts990@gmail.com>
2024-12-19 12:15:08 +0500

    auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,36 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
+import { Fragment, useEffect, useState } from "react"
+import { supabase } from "./config/supabase"
+
+export const get_user = async () => {
+  const { data: { user } } = await supabase.auth.getUser()
+  return user
+}
 
 const App = () => {
+  const [user, set_user] = useState<any>(null)
 
-  
+  useEffect(() => {
+    const user = get_user()
+    set_user(user)
+  }, [])
 
   return (
     <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/login" replace={true} />} />
+      {
+        user ?
+          <Fragment>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" replace={true} />} />
+          </Fragment> :
+          <Fragment>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/login" replace={true} />} />
+          </Fragment>
+      }
     </Routes>
   )
 }

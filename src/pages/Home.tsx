e08e2742fd94eb7349d81644da417b7b646a71e1
ahadsts9901 <ahadsts990@gmail.com>
2024-12-19 12:10:51 +0500

    logout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
+import { useNavigate } from "react-router-dom"
+import { supabase } from "../config/supabase"
 import "./index.css"
 
 const Home = () => {
+    const navigate = useNavigate()
+
+    const logout = async () => {
+        const { error } = await supabase.auth.signOut()
+        if (error) {
+            alert(error)
+        } else {
+            alert("Logout successfull")
+            navigate("/login")
+        }
+    }
+
     return (
-        <div>Home</div>
+        <div className="main">
+            <div className="header">
+                <button onClick={logout}>Logout</button>
+            </div>
+        </div>
     )
 }
 
